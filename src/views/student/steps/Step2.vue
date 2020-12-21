5a677d196feece94d87b1e0f9611d226ff52596d
vasin sermsampan <dev.vasin@gmail.com>
2020-12-21 21:18:39 +0700

    fix(*): fix step 2,3 type error

--- src/views/student/steps/Step2.vue
+++ src/views/student/steps/Step2.vue
@@ -198,7 +198,8 @@ export default class Step2 extends SDashboard {
 
   // on file change
   fileChange(name, file) {
-    if (file) {
+    if (file && file[0]) {
+      console.log(file);
       const file_type = file[0].type;
       if (file_type !== 'application/pdf') {
         return this.$swal({
